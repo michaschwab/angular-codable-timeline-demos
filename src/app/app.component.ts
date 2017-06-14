6b8981b5598931bbd3b3216e7f1953321f85ecda
Michail Schwab <me@michaschwab.de>
2017-06-14 09:16:30 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  start = new Date(2000, 1);
+  end = new Date();
+  margin = {top: 20, right: 20, bottom: 20, left: 20};
+  public x;
+
+  onScaleUpdate(fct)
+  {
+    this.x = fct;
+  }
+
+  events = [{name: 'Something', description: 'March 2001', time: new Date(2001, 3)}, {name: 'Something else', description: 'January 2010', time: new Date(2010, 1)}];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CodableTimelineModule } from 'angular-codable-timeline';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CodableTimelineModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
